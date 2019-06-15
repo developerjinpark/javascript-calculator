cc93e2f2e5cbcd0702aee4641b6ba470c6545708
Jin <jinpark1504@gmail.com>
2019-06-14 22:42:50 -0400

    Handled operator display and added digital font

--- src/App.js
+++ src/App.js
@@ -12,18 +12,46 @@ class App extends React.Component {
   }
   handleClick(e) {
     // console.log((/number/).test(e.target.className));
-    // console.log(e.target);
-    // if ((/number/).test(e.target.className)) {
-    //   if (this.state.result !== '0') {
-    //     this.setState({
-    //       result: this.state.result + e.target.value
-    //     });
-    //   } else if (e.target.value !== '0') {
-    //     this.setState({
-    //       result: e.target.value
-    //     });
-    //   }
-    // } 
+    // console.log(e.target.value);
+    if (e.target.value === 'AC') {
+      this.setState({
+        input: '',
+        result: '0'
+      })
+    }
+    if ((/number/).test(e.target.className)) {
+      if (this.state.result !== '0') {
+        this.setState({
+          input: this.state.input + e.target.value,
+          result: this.state.result + e.target.value
+        });
+      } else if (e.target.value !== '0') {
+        this.setState({
+          input: e.target.value,
+          result: e.target.value
+        });
+      }
+    }
+    
+    // console.log(this.state.result.length);
+    // console.log(this.state.result[this.state.result.length -1]);
+    if ((/operator/).test(e.target.className)) {
+      if (this.state.result !== '0') {
+        // console.log((/[+-/x]/).test(this.state.result[this.state.result.length - 1]));
+        if (!(/[+-/x]/).test(this.state.result[this.state.result.length - 1])) {
+          this.setState({
+            input: this.state.input + e.target.value,
+            result: this.state.result + e.target.value
+          });
+        } else {
+          // console.log('here, ' + this.state.input.slice(0, this.state.input.length - 1));
+          this.setState({
+            input: this.state.input.slice(0, this.state.input.length - 1) + e.target.value.replace(/x/, '*'),
+            result: this.state.input.slice(0, this.state.input.length - 1) + e.target.value.replace(/x/, '*')
+          });
+        }
+      }
+    }
   }
 
   render() {
@@ -35,23 +63,23 @@ class App extends React.Component {
             <p id="displayCurrent">{this.state.result}</p>
           </div>
           <div id="keyPads">
-            <div id="clear" className="grid" onClick={this.handleClick}><p>AC</p></div>
-            <div id="divide" className="grid operator" onClick={this.handleClick}>/</div>
-            <div id="multiply" className="grid operator" onClick={this.handleClick}>X</div>
-            <div id="seven" className="grid number" onClick={this.handleClick}>7</div>
-            <div id="eight" className="grid number" onClick={this.handleClick}>8</div>
-            <div id="nine" className="grid number" onClick={this.handleClick}>9</div>
-            <div id="subtract" className="grid operator" onClick={this.handleClick}>-</div>
-            <div id="four" className="grid number" onClick={this.handleClick}>4</div>
-            <div id="five" className="grid number" onClick={this.handleClick}>5</div>
-            <div id="six" className="grid number" onClick={this.handleClick}>6</div>
-            <div id="add" className="grid operator" onClick={this.handleClick}>+</div>
-            <div id="one" className="grid number" onClick={this.handleClick}>1</div>
-            <div id="two" className="grid number" onClick={this.handleClick}>2</div>
-            <div id="three" className="grid number" onClick={this.handleClick}>3</div>
-            <div id="equals" className="grid" onClick={this.handleClick}>=</div>
-            <div id="zero" className="grid number" onClick={this.handleClick}>0</div>
-            <div id="decimal" className="grid number" onClick={this.handleClick}>.</div>
+            <button id="clear" className="grid" onClick={this.handleClick} value="AC">AC</button>
+            <button id="divide" className="grid operator" onClick={this.handleClick} value="/">/</button>
+            <button id="multiply" className="grid operator" onClick={this.handleClick} value="x">x</button>
+            <button id="seven" className="grid number" onClick={this.handleClick} value="7">7</button>
+            <button id="eight" className="grid number" onClick={this.handleClick} value="8">8</button>
+            <button id="nine" className="grid number" onClick={this.handleClick} value="9">9</button>
+            <button id="subtract" className="grid operator" onClick={this.handleClick} value="-">-</button>
+            <button id="four" className="grid number" onClick={this.handleClick} value="4">4</button>
+            <button id="five" className="grid number" onClick={this.handleClick} value="5">5</button>
+            <button id="six" className="grid number" onClick={this.handleClick} value="6">6</button>
+            <button id="add" className="grid operator" onClick={this.handleClick} value="+">+</button>
+            <button id="one" className="grid number" onClick={this.handleClick} value="1">1</button>
+            <button id="two" className="grid number" onClick={this.handleClick} value="2">2</button>
+            <button id="three" className="grid number" onClick={this.handleClick} value="3">3</button>
+            <button id="equals" className="grid" onClick={this.handleClick} value="=">=</button>
+            <button id="zero" className="grid number" onClick={this.handleClick} value="0">0</button>
+            <button id="decimal" className="grid number" onClick={this.handleClick} value=".">.</button>
           </div>
         </div>
         <footer>
